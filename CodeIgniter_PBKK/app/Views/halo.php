0d4178750b759e0da0eaf4a451ef36260a3b8b7f
valen fajri <114421539+valenfajri18@users.noreply.github.com>
2023-11-08 17:36:51 +0700

    Add files via upload

---
+++ CodeIgniter_PBKK/app/Views/halo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Code Igniter 4</title>
+</head>
+
+<body>
+    <?php
+    echo "<h1>Hello World</h1><br>";
+    echo "<h2>Namaku Layyinatul Fuadah</h2><br>";
+    ?>
+</body>
+
+</html>
